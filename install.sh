246971ba4ce3e854c6aa9835049a2c66fff6b9e1
ryuuwiz <ryuuwiz@gmail.com>
2020-02-12 18:28:28 +0700

    Add Clean Command to clear the package

--- install.sh
+++ install.sh
@@ -11,16 +11,24 @@ echo "---------------------------------------"
 
 # Download Packages
 echo "Downloading Packages....."
-wget http://archive.ubuntu.com/ubuntu/pool/universe/g/gksu/gksu_2.0.2-9ubuntu1_amd64.deb
-wget http://ftp.br.debian.org/debian/pool/main/libg/libgksu/libgksu2-0_2.0.13~pre1-9+b1_amd64.deb
-wget http://ftp.br.debian.org/debian/pool/main/libg/libgtop2/libgtop-2.0-10_2.34.2-1_amd64.deb
+sudo wget -o /tmp/gksu.deb http://archive.ubuntu.com/ubuntu/pool/universe/g/gksu/gksu_2.0.2-9ubuntu1_amd64.deb
+sudo wget -o /tmp/libgksu.deb http://ftp.br.debian.org/debian/pool/main/libg/libgksu/libgksu2-0_2.0.13~pre1-9+b1_amd64.deb
+sudo wget -o /tmp/libgtop.deb http://ftp.br.debian.org/debian/pool/main/libg/libgtop2/libgtop-2.0-10_2.34.2-1_amd64.deb
 
 # Installing Packages
 echo "Installing Packages...."
-sudo dpkg -i --force-all libgtop*
-sudo dpkg -i --force-all libgksu*
-sudo dpkg -i --force-all gksu*
+sudo dpkg -i --force-all /tmp/libgtop.deb
+sudo dpkg -i --force-all /tmp/libgksu.deb
+sudo dpkg -i --force-all /tmp/gksu.deb
 sudo apt-get install -f -y
 
-echo ""
-echo "Thanks for Using this Script...."
+# Cleaning Package
+sudo rm -rf /tmp/libgtop.deb
+sudo rm -rf /tmp/libgksu.deb
+sudo rm -rf /tmp/gksu.deb
+
+echo "----------------------------------"
+echo "                                  "
+echo "	Thanks for Using this Script... "
+echo "                                  "
+echo "----------------------------------"
